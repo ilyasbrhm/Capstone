2515af39bf15ec46fe504e678ca650d680bafa37
ilyasbrhm <ilyasmules.9803@gmail.com>
2025-06-09 16:38:28 +0700

    Delete Backend/moodflix-api-capstone-main directory

--- Backend/moodflix-api-capstone-main/utils/helpers.js
+++
@@ -1,9 +0,0 @@
-const formatMood = (mood) => {
-    if (!mood) return '';
-    // Capitalize first letter, lowercase the rest
-    return mood.charAt(0).toUpperCase() + mood.slice(1).toLowerCase();
-  };
-  
-  module.exports = {
-    formatMood
-  };
